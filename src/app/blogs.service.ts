710581ae915cc6635a59d4b79ab4e24478717868
Joash <joash.n09@gmail.com>
2022-08-03 21:52:49 +0200

    Add Jispy - Lisp Interpreter web app

--- src/app/blogs.service.ts
+++ src/app/blogs.service.ts
@@ -6,17 +6,18 @@ import { Injectable } from '@angular/core';
 export class BlogsService {
 
   BlogPosts: BlogPost[] = [
+    { id: 'WebAssembly', title: 'Porting my Lisp Interpreter (written in C) to Webassembly', date: '02-08-2022', thumbnail: 'webassembly.png' },
     { id: 'MultipleKernels', title: 'Managing Multiple Linux Kernel Configurations', date: '30-07-2022', thumbnail: 'kernel.jpg' },
     { id: 'Chroot', title: 'Chroot jail and systemd-nspawn containers', date: '11-07-2022', thumbnail: 'jail.jpg' },
     { id: 'DeploySite', title: 'How I deployed this website', date: '11-07-2022', thumbnail: 'website.jpg' },
-    { id: 'Bonds', title: 'What I learnt about financial bonds', date: '05-04-2021', thumbnail: 'bond.jpg' },
     { id: 'PostgresSQLEnum', title: 'PostgreSQL Enums', date: '24-07-2022', thumbnail: 'postgres.jpg'},
-    { id: 'Fallacies', title: 'Identifying my biases and fallacies', date: '10-04-2021', thumbnail: 'thinking.jpg' },
-    { id: 'Docker', title: 'Docker for development', date: '09-09-2021', thumbnail: 'docker.jpg' },
-    { id: 'Microblaze', title: 'Microblaze softcore on Arty A7', date: '09-09-2021', thumbnail: 'microblaze.png' },
     { id: 'ORM', title: 'Working with ORM, DB Migrations and Repository Patterns', date: '09-09-2021', thumbnail: 'objectmapping.jpg' },
+    { id: 'Microblaze', title: 'Microblaze softcore on Arty A7', date: '09-09-2021', thumbnail: 'microblaze.png' },
+    { id: 'Docker', title: 'Docker for development', date: '09-09-2021', thumbnail: 'docker.jpg' },
     { id: 'AddSwagger', title: 'Adding SwaggerUI to ASP.NET Core', date: '31-07-2021', thumbnail: 'swagger.png' },
     { id: 'IPFS', title: 'A decentralized web', date: '31-07-2021', thumbnail: 'ipfs.png' },
+    { id: 'Fallacies', title: 'Identifying my biases and fallacies', date: '10-04-2021', thumbnail: 'thinking.jpg' },
+    { id: 'Bonds', title: 'What I learnt about financial bonds', date: '05-04-2021', thumbnail: 'bond.jpg' },
   ];
 
   constructor() { }
--- src/app/webapp/jispy/jispy.component.ts
+++ src/app/webapp/jispy/jispy.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject, OnDestroy } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { ThemeService } from 'src/app/theme.service';
 
 @Component({
   selector: 'app-jispy',
   templateUrl: './jispy.component.html',
   styleUrls: ['./jispy.component.css']
 })
-export class JispyComponent implements OnInit {
+export class JispyComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isDark = false;
+  subscription = new Subscription();
+
+  constructor(private _render: Renderer2,
+              @Inject(DOCUMENT) private _document: Document,
+              private _themeService: ThemeService
+             ) { }
 
   ngOnInit(): void {
+
+    this.subscription.add(this._themeService.isDarkMode$().subscribe((result: boolean) => {
+      this.isDark = result;
+    }));
+
+    this._loadScript('jispy_overrides');
+    this._loadScript('jispy');
+  }
+
+  ngOnDestroy(): void {
+    this._document.getElementById('jispy_overrides')?.remove();
+    this._document.getElementById('jispy')?.remove();
+
+    this.subscription.unsubscribe();
+  }
+
+  private _loadScript(name: string) {
+
+    if (this._document.getElementById(name)) { return; }
+
+    let script = this._render.createElement('script') as HTMLScriptElement;
+    script.id = name;
+    script.type = 'text/javascript';
+    script.src = 'assets/jispy/'+name+'.js';
+    script.async = true;
+    script.defer = true;
+    script.onload = () => {
+      console.log('Script loaded')
+    }
+    this._render.appendChild(this._document.body, script);
+
   }
 
 }
