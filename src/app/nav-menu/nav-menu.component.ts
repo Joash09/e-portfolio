f052da2e96838d19670098506e3e35c018be6413
Joash <joash.n09@gmail.com>
2022-08-03 22:38:53 +0200

    Toggle theme icon

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { faEnvelope, faRocket, faMoon, faGift } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { faEnvelope, faRocket, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { ThemeService } from '../theme.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent implements OnInit {
+export class NavMenuComponent implements OnInit, OnDestroy {
 
   isExpanded = false;
+  isDark = false;
   faRocket = faRocket;
   faGithub = faGithub;
   faEnvelope = faEnvelope;
   faMoon = faMoon;
+  faSun = faSun;
+
+  subscription = new Subscription();
 
   constructor(private _themeService: ThemeService) { }
 
   ngOnInit(): void {
+    this.subscription.add(this._themeService.isDarkMode$().subscribe((result: boolean) => {
+      this.isDark = result;
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   toggleTheme(): void {
